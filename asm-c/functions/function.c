64a4b1a48ead619b7656b35a033c043495a7d435
vrstanchev <vrstanchev@gmail.com>
2024-05-19 04:32:30 -0400

    Educational materials for functions and reduce of register usage added

---
+++ asm-c/functions/function.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int plus(int a,int b){
+int c=a+b;
+printf("Result is:\n");
+printf("%d\n",c);
+}
+int minus(int a,int b){
+int c=a-b;
+printf("Result is:\n");
+printf("%d \n",c);
+}
+int devide(int a,int b){
+int c=a/b;
+printf("Result is:\n");
+printf("%d \n",c);
+
+}
+int multiply(int a,int b){
+int c=a*b;
+printf("Result is:\n");
+printf("%d \n",c);
+
+}
+
--- asm-c/functions/program.c
+++ asm-c/functions/program.c
@@ -1,44 +1,20 @@
 #include <stdio.h>
 #include "program.h"
 int main(void){
-int a,b,sw;
+int a,b;
 printf("Enter a:\n");
 scanf("%d",&a);
 printf("Enter b:\n");
 scanf("%d",&b);
-printf("Enter choise from 1 to 4:\n");
-scanf("%d",&sw);
-switch(sw){
-case 1: plus(a,b);
-case 2: minus(a,b);
-case 3: devide(a,b);
-case 4: multiply(a,b);
-default: printf("Exit...\n");
-}
-
+//plus
+plus(a,b);
+//minus
+minus( a,b);
+//devide
+devide(a,b);
+//multiply
+multiply(a,b);
 return (0);
-
-}
-int plus(int a,int b){
-int c=a+b;
-printf("Result is: %d \n",c);
-
-}
-int minus(int a,int b){
-int c=a-b;
-printf("Result is: %d \n",c);
-
-}
-
-int devide(int a,int b){
-int c=a/b;
-printf("Result is: %d \n",c);
-
-}
-
-int multiply(int a,int b){
-int c=a*b;
-printf("Result is: %d \n",c);
 
 }
 
