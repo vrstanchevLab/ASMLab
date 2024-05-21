ac92838ea5e912a5137b08c5c7162b13b2ccaca3
vrstanchev <vrstanchev@gmail.com>
2024-05-21 15:01:03 -0400

    Educational materials for functions and reduce of register usage added

---
+++ Basics/functions/additional.asm
@@ -0,0 +1,4 @@
+mov eax,1 ;store into register eax, value of 1  
+mov ebx,0 ;store into register ebx, value of 0  
+cmp eax, ebx ;compare the values of eax and ebx
+
--- Basics/functions/function.asm
+++ Basics/functions/function.asm
@@ -1,20 +1,15 @@
-.text ;code section starts here 
+%include 'additional.asm'
+section .text ;code section starts here 
 global _start ;global label _start definition  
 _start:  ;global label _start is defined and it's action starts here  
-mov eax,1 ;store into register eax, value of 1  
-mov ebx.0 ;store into register ebx, value of 0  
-cmp eax, ebx ;compare the values of eax and ebx
 jge greater  ;if the value of compare statement is logical 1, if eax is greater than ebx, then jump to the to the function called greater
-jle lower   ;if the value of compare statement is logical 1, if eax is loweer than ebx, then jump to the to the function called lower
-je equal   ; if the value of compare statement is logical 1, if eax is equal to the value of  ebx, then jump to the to the function called equal
-;the function definitions starts below:  
 greater: ;the function greater starts here
 mov eax,2 ;store into the register eax value of 2
-int 0x80 ;make a system call to the global label _start
+jle lower   ;if the value of compare statement is logical 1, if eax is loweer than ebx, then jump to the to the function called lower
 lower:;the function greater starts here
 mov eax,1 ;store into the register eax value of 1
-int 0x80  ;make a system call to the global label _start
-equal:;the function greater starts here
+je equal1   ; if the value of compare statement is logical 1, if eax is equal to the value of  ebx, then jump to the to the function called equal
+equal1:;the function greater starts here
 mov eax, 0 ;store into the register eax value of 0
 int 0x80  ;make a system call to the global label _start
 
